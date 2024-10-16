43aff7014d967fe5b3d0353d5cdfb1488f3084bc
leoldding <leo.ld.ding@gmail.com>
2024-10-16 14:19:46 -0400

    projects style

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -10,7 +10,7 @@ const About: React.FC = () => {
                 I'm a firm believer in continually learning new things. Even after graduating from NYU with a degree in Computer Science and Data Science, I've picked up cloud technology certifications and continue to build projects to hone my typing skills.
             </p>
             <p>
-                When I'm not coding, I'm hitting the gym, watching motorsports, solving Rubik's Cubes, or cleaning up a (virtual) minefield.
+                When I'm not coding, I'm hitting the gym, watching motorsports, solving Rubik's Cubes, or cleaning up (virtual) minefields.
             </p>
         </section>
     );
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import data from "../data/projects.json";
+import "../styles/projects.css";
 
 const Projects: React.FC = () => {
     const [showMore, setShowMore] = useState(false);
@@ -16,29 +17,34 @@ const Projects: React.FC = () => {
             <h1>
                 Projects
             </h1>
-            <Link to="/archive">View Archive</Link>
-            {projects && projects.map(({ id, name, technologies, github, link, description }) => (
-                <div key={id}>
-                    <p>
-                        {name}
-                    </p>
-                    <p>
-                        {technologies && technologies.map(tech => (
-                            <span key={tech}>{tech} </span>
-                        ))}
-                    </p>
-                    <p>
-                        <Link to={github} target="_blank" rel="noopener noreferrer">github</Link>
-                    </p>
-                    {link && <p>
-                        <Link to={link} target="_blank" rel="noopener noreferrer">website</Link>
-                    </p>}
-                    <p>
-                        {description}
-                    </p>
-                </div>
-            ))}
-            <button onClick={handleShowProjects}>{showMore ? "Show Less" : "Show More"}</button>
+            <div className="projects">
+                {projects && projects.map(({ id, name, technologies, github, link, description }) => (
+                    <div className="project" key={id}>
+                        <div className="project-shadow" />
+                        <div className="project-info">
+                            <p>
+                                {name}
+                            </p>
+                            <p>
+                                {technologies && technologies.map(tech => (
+                                    <span className="project-tech" key={tech}>{tech} </span>
+                                ))}
+                            </p>
+                            <p>
+                                <Link to={github} target="_blank" rel="noopener noreferrer">github</Link>
+                            </p>
+                            {link && <p>
+                                <Link to={link} target="_blank" rel="noopener noreferrer">website</Link>
+                            </p>}
+                            <p>
+                                {description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+                <button onClick={handleShowProjects}>{showMore ? "Show Less" : "Show More"}</button>
+                <Link to="/archive">View Archive</Link>
+            </div>
         </section>
     );
 };

2ce200434870ffd9390d40bcd27426f6afcaa84b
leoldding <leo.ld.ding@gmail.com>
2024-10-17 14:42:30 -0400

    project card format

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import data from "../data/projects.json";
 import "../styles/projects.css";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 const Projects: React.FC = () => {
     const [showMore, setShowMore] = useState(false);
@@ -22,23 +23,27 @@ const Projects: React.FC = () => {
                     <div className="project" key={id}>
                         <div className="project-shadow" />
                         <div className="project-info">
-                            <p>
-                                {name}
-                            </p>
-                            <p>
-                                {technologies && technologies.map(tech => (
-                                    <span className="project-tech" key={tech}>{tech} </span>
-                                ))}
-                            </p>
-                            <p>
-                                <Link to={github} target="_blank" rel="noopener noreferrer">github</Link>
-                            </p>
-                            {link && <p>
-                                <Link to={link} target="_blank" rel="noopener noreferrer">website</Link>
-                            </p>}
+                            <div className="project-header">
+                                <h2>
+                                    {name}
+                                </h2>
+                                <div className="project-links">
+                                    <div>
+                                        <Link to={github} target="_blank" rel="noopener noreferrer"><FaGithub  /></Link>
+                                    </div>
+                                    {link && <div>
+                                        <Link to={link} target="_blank" rel="noopener noreferrer"><FaExternalLinkAlt /></Link>
+                                    </div>}
+                                </div>
+                            </div>
                             <p>
                                 {description}
                             </p>
+                            <div>
+                                {technologies && technologies.map(tech => (
+                                    <span className="project-tech" key={tech}>{tech} </span>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 ))}

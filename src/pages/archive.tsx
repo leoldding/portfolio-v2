db9c2db671752347e7ae8a95e91b5e4acff4be11
leoldding <leo.ld.ding@gmail.com>
2024-10-19 12:13:19 -0400

    archive table

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import projects from "../data/projects.json";
+import "../styles/archive.css";
 
 const Archive: React.FC = () => {
     return (
         <div className="archive-container">
-            <Link to="/">Leo Ding</Link>
-            {projects && projects.map(({ id, name, technologies, github, link, year }) => (
-                <div key={id}>
-                    {year} - {name} | {technologies && technologies.map(tech => (<span key={tech}>{tech} </span>))} | <Link to={github} target="_blank">github</Link> | {link && <Link to={link} target="_blank">website</Link>}
+            <Link to="/">← Leo Ding</Link>
+            <div className="archive-table">
+                <div className="archive-header">
+                    <div>Year</div>
+                    <div>Name</div>
+                    <div>Technologies</div>
+                    <div>GitHub</div>
+                    <div>Link</div>
                 </div>
-            ))}
+                {projects && projects.map(({ id, name, technologies, github, link, year }) => (
+                    <div className="archive-row" key={id}>
+                        <div>{year}</div>
+                        <div>{name}</div>
+                        <div className="archive-tags">{technologies && technologies.map(tech => (<span className="archive-tech" key={tech}>{tech} </span>))}</div>
+                        <div><Link to={github} target="_blank">github</Link></div>
+                        <div>{link && <Link to={link} target="_blank">website</Link>}</div>
+                    </div>
+                ))}
+            </div>
         </div >
     );
 };

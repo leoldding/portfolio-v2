34b0e2d8d02537abeef479180a286f324745a03f
leoldding <leo.ld.ding@gmail.com>
2024-10-23 14:46:30 -0400

    cert styling

--- src/components/certifications.tsx
+++ src/components/certifications.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import certifications from "../data/certifications.json";
+import "../styles/certifications.css";
 
 const Certifications: React.FC = () => {
     return (
@@ -7,11 +8,30 @@ const Certifications: React.FC = () => {
             <h1>
                 Certifications
             </h1>
-            {certifications && certifications.map(({ id, name, short, company, issued }) => (
-                <p key={id}>
-                        {name} ({short}), {company} - Issued {issued}
-                </p>
-            ))}
+            <div className="certifications">
+                {certifications && certifications.map(({ id, name, short, company, issued }) => (
+                    <div className="certification" key={id}>
+                        <div className="certification-shadow" />
+                        <div className="certification-info">
+                            <div className="certification-name">
+                                <h2>
+                                    {name} ({short})
+                                </h2>
+                            </div>
+                            <div className="certification-company">
+                                <h3>
+                                    {company}
+                                </h3>
+                            </div>
+                            <div className="certification-date">
+                                <p>
+                                    Issued {issued}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 };

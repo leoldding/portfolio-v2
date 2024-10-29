247fe9be31d54b3ccd708e610c833c34d0818fa1
leoldding <leo.ld.ding@gmail.com>
2024-10-29 15:15:35 -0400

    about links

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/about.css";
 
 const About: React.FC = () => {
     return (
@@ -10,7 +12,7 @@ const About: React.FC = () => {
                 I'm a firm believer in continually learning new things. Even after graduating from NYU with a degree in Computer Science and Data Science, I've picked up cloud technology certifications and continue to build projects to hone my typing skills.
             </p>
             <p>
-                When I'm not coding, I'm hitting the gym, watching motorsports, solving Rubik's Cubes, or cleaning up (virtual) minefields.
+                When I'm not coding, I'm hitting the gym, watching motorsports, solving <Link to="https://www.worldcubeassociation.org/persons/2019DING10" target="_blank" rel="noopener noreferrer" id="cube-cursor">Rubik's Cubes</Link>, or cleaning up (virtual) <Link to="https://minesweeper.online/player/10436085" target="_blank" rel="noopener noreferrer">minefields</Link>.
             </p>
         </section>
     );

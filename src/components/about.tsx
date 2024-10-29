1089054f5eab64d4afb374278e9fefaf24618278
leoldding <leo.ld.ding@gmail.com>
2024-10-29 16:49:38 -0400

    custom cursor

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -12,7 +12,7 @@ const About: React.FC = () => {
                 I'm a firm believer in continually learning new things. Even after graduating from NYU with a degree in Computer Science and Data Science, I've picked up cloud technology certifications and continue to build projects to hone my typing skills.
             </p>
             <p>
-                When I'm not coding, I'm hitting the gym, watching motorsports, solving <Link to="https://www.worldcubeassociation.org/persons/2019DING10" target="_blank" rel="noopener noreferrer" id="cube-cursor">Rubik's Cubes</Link>, or cleaning up (virtual) <Link to="https://minesweeper.online/player/10436085" target="_blank" rel="noopener noreferrer">minefields</Link>.
+                When I'm not coding, I'm hitting the gym, watching motorsports, solving <Link id="cube-cursor" to="https://www.worldcubeassociation.org/persons/2019DING10" target="_blank" rel="noopener noreferrer">Rubik's Cubes</Link>, or cleaning up (virtual) <Link id="mine-cursor" to="https://minesweeper.online/player/10436085" target="_blank" rel="noopener noreferrer">minefields</Link>.
             </p>
         </section>
     );
